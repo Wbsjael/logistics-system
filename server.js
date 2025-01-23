bcecabccedd3f119da11fc7017e48cb6c09b6d80
wbsjael <muchemijessicas77@gmail.com>
2025-01-23 12:35:45 +0300

    Add .env file

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-// server.js or your main backend file
+require('dotenv').config();
 const mongoose = require('mongoose');
-require('dotenv').config(); // This loads the environment variables
 
-// Make sure your MONGO_URI environment variable is set correctly
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log("MongoDB connection error: ", err));
+const uri = process.env.MONGO_URI;
+if (!uri) {
+    console.error('Error: MONGO_URI is not defined in .env file');
+    process.exit(1);  // Exit the application if the URI is missing
+}
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 
